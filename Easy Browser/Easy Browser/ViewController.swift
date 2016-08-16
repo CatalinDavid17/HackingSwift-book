d6b8321e73ff8ef9afa071cd1ab7383b06edbc13
Catalin David <catalindavid@SL45-205.softvision.ro>
2016-08-16 17:29:08 +0300

    - easyBrowser use of WebKit - added barButton to open a specific page - set the title to the webpage's

---
+++ Easy Browser/Easy Browser/ViewController.swift
@@ -0,0 +1,74 @@
+//
+//  ViewController.swift
+//  Easy Browser
+//
+//  Created by Catalin David on 16/08/16.
+//  Copyright © 2016 Catalin David. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKNavigationDelegate {
+
+	var webView: WKWebView!
+	
+	override func loadView() {
+		
+		webView = WKWebView()
+		webView.navigationDelegate = self
+		view = webView
+		
+	}
+	
+	override func viewDidLoad() {
+		
+		super.viewDidLoad()
+		
+		let url = NSURL(string: "https://www.hackingwithswift.com")!
+		webView.loadRequest(NSURLRequest(URL: url))
+		webView.allowsBackForwardNavigationGestures = true
+		
+		navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open",
+		                                                    style: .Plain,
+		                                                    target: self,
+		                                                    action: #selector(openTapped))
+		
+	}
+	
+	func webView(webView: WKWebView, didFinishNavigation navigation: WKNavigation!) {
+		
+		title = webView.title
+		
+	}
+	
+	func openPage(action: UIAlertAction!) {
+	
+		let url = NSURL(string: "https://" + action.title!)!
+		webView.loadRequest(NSURLRequest(URL: url))
+		
+	}
+	
+	func openTapped() {
+	
+		let alertController = UIAlertController(title: "Open page...",
+		                                        message: nil,
+		                                        preferredStyle: .ActionSheet)
+		alertController.addAction(UIAlertAction(title: "hackingwithswift.com",
+												style: .Default,
+												handler: openPage))
+		alertController.addAction(UIAlertAction(title: "apple.com",
+												style: .Default,
+												handler: openPage))
+		alertController.addAction(UIAlertAction(title: "Cancel",
+												style: .Cancel,
+												handler: nil ))
+		
+		presentViewController(alertController, animated: true, completion: nil)
+		
+	}
+	
+	
+	
+}
+
