1e4c9c6449ef9e7ed259fd7ef809a2ab3f6f07e1
Catalin David <catalindavid@SL45-205.softvision.ro>
2016-08-17 18:20:37 +0300

    Project8 - First commit - added interface configuration + outlets/actions

---
+++ Project8 - 7 swifty words/Project8 - 7 swifty words/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Project8 - 7 swifty words
+//
+//  Created by Catalin David on 17/08/16.
+//  Copyright © 2016 Catalin David. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	@IBOutlet weak var scoreLabel: UILabel!
+	@IBOutlet weak var currentAnswer: UITextField!
+	@IBOutlet weak var cluesLabel: UILabel!
+	@IBOutlet weak var answersLabel: UILabel!
+	
+	override func viewDidLoad() {
+	
+		super.viewDidLoad()
+	
+	}
+
+	//MARK: Methods for action Buttons
+	@IBAction func submitTapped(sender: UIButton) {
+		
+		
+		
+	}
+	@IBAction func clearTapped(sender: UIButton) {
+		
+		
+		
+	}
+
+}
+
