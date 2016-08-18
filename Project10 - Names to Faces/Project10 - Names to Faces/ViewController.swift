c1429ce7112c380fa7044f43cf48cde0a7d070fb
Catalin David <catalindavid@SL45-205.softvision.ro>
2016-08-18 15:14:03 +0300

    - added addNewPerson method : lets the user to pick an image using UIImagePickerController, sets the viewcontroller as its delegate - added getDocumentsDirectory method: provides a valid path within app's DocumentDirectory appending an unique id generated using NSUUID - added imagePickerControllerDidCancel, imagePickerController: didFinishPickingMediaWithInfo:  we get the image from the passed dictionary, we convert it into NSData and write it to the specified path

--- Project10 - Names to Faces/Project10 - Names to Faces/ViewController.swift
+++ Project10 - Names to Faces/Project10 - Names to Faces/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
 	@IBOutlet weak var collectionView: UICollectionView!
 	
@@ -16,8 +16,79 @@ class ViewController: UIViewController {
 	
 		super.viewDidLoad()
 		
+		navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Add,
+		                                                   target: self,
+		                                                   action: #selector(addNewPerson))
 		
 	}
-
+	
+	//MARK: - Methods
+	
+	func addNewPerson() {
+		
+		let picker = UIImagePickerController()
+		picker.allowsEditing = true
+		picker.delegate = self
+		presentViewController(picker, animated: true, completion: nil)
+		
+	}
+	
+	func getDocumentsDirectory() -> NSString {
+		
+		let paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)
+		let documentDirectory = paths[0]
+		
+		return documentDirectory
+		
+	}
+	
+	//MARK: - UiPickerControllerDelegate
+	
+	func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+		
+		dismissViewControllerAnimated(true, completion: nil)
+		
+	}
+	
+	func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+		
+		var newImage: UIImage
+		
+		if let possibleImage = info[UIImagePickerControllerEditedImage]	as? UIImage {
+			newImage = possibleImage
+		} else if let possibleImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+			newImage = possibleImage
+		} else {
+			print("We cannot find an image!")
+			return
+		}
+		
+		let imageName = NSUUID().UUIDString
+		let imagePath = getDocumentsDirectory().stringByAppendingPathComponent(imageName)
+		
+		if let jpegData = UIImageJPEGRepresentation(newImage, 80) {
+			jpegData.writeToFile(imagePath, atomically: true)
+		}
+		
+		dismissViewControllerAnimated(true, completion: nil)
+		
+	}
+	
+	//MARK: - UICollectionViewDataSource - UICollectionViewDelegate
+	
+	func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+		
+		return 10
+		
+	}
+	
+	func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+		
+		let cell = collectionView.dequeueReusableCellWithReuseIdentifier("Person", forIndexPath: indexPath) as! CDPersonCell
+		
+		return cell
+		
+	}
+	
 }
 
