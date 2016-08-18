ba940379394bb7c71c4f06687566b793f6831c38
Catalin David <catalindavid@SL45-205.softvision.ro>
2016-08-18 13:00:16 +0300

    Project 10 - first commit - configured interface : added collection view, custom class for collectionViewCell, connected outlets

---
+++ Project10 - Names to Faces/Project10 - Names to Faces/CDPersonCell.swift
@@ -0,0 +1,17 @@
+//
+//  CDPersonCell.swift
+//  Project10 - Names to Faces
+//
+//  Created by Catalin David on 18/08/16.
+//  Copyright © 2016 Catalin David. All rights reserved.
+//
+
+import UIKit
+
+class CDPersonCell: UICollectionViewCell {
+	
+	@IBOutlet weak var imageView: UIImageView!
+	@IBOutlet weak var name: UILabel!
+	
+	
+}
---
+++ Project10 - Names to Faces/Project10 - Names to Faces/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Project10 - Names to Faces
+//
+//  Created by Catalin David on 18/08/16.
+//  Copyright © 2016 Catalin David. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	@IBOutlet weak var collectionView: UICollectionView!
+	
+	override func viewDidLoad() {
+	
+		super.viewDidLoad()
+		
+		
+	}
+
+}
+
