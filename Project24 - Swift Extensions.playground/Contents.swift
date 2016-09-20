b613977c10463f3114afc994c3e06c82aaf40e3f
Alin Petrus <alin.petrus@softvision.ro>
2016-09-20 18:03:44 +0300

    - project 25 - multipeer

---
+++ Project24 - Swift Extensions.playground/Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+extension Int {
+	mutating func plusOne() -> Int {
+		return self + 1
+	}
+}
+
+var myInt = 0
+
+myInt.plusOne()
+
